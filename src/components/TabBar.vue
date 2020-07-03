b13fe9af12217448f735e6283e53b6e388491500
McCall <1059511459@qq.com>
2020-07-04 00:03:43 +0800

    init tabbar

---
+++ src/components/TabBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul class="tabUl">
+            <li>百 度</li>
+            <li>淘 宝</li>
+            <li>京 东</li>
+            <li>知 乎</li>
+            <li>B 站</li>
+            <li>B 站</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+    @import "../assets/css/tabbar.css";
+</style>
